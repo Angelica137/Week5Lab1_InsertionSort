4a24da03ca373f2e6a20ffa8f579e0f77691ebe3
Angelica <Angelica137@users.noreply.github.com>
2021-04-10 15:44:19 +0100

    .sort() acceots an array as an argument

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,5 +1,7 @@
 class InsertionSort {
-	public void sort() {
+	// accepts an array of ints as anrgument and sorts it
+	// in ascending order
+	public void sort(int[] array) {
 		System.out.println("Hello, world!");
 	}
 
