806839668b9c4c943c06a23f38cae6f7c3e92280
Angelica <Angelica137@users.noreply.github.com>
2021-04-10 15:56:15 +0100

    fix .sort() so it actually returns array vs just printing it

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 class InsertionSort {
 	// accepts an array of ints as anrgument and sorts it
 	// in ascending order
-	public void sort(int[] array) {
-		System.out.println(Arrays.toString(array));
+	public int[] sort(int[] array) {
+		return array;
 	}
 
 }
--- TestInsertionSort.java
+++ TestInsertionSort.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 class TestInsertionSort {
 	public static void main(String[] args) {
 		InsertionSort i = new InsertionSort();
 		int[] a = { 1, 2, 3 };
-		i.sort(a);
+		System.out.println(Arrays.toString(i.sort(a)));
 
 	}
 
