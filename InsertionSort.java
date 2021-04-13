36ae9a45b16111bd5ee84a9a53bfca52d3be068c
Angelica <Angelica137@users.noreply.github.com>
2021-04-13 23:21:16 +0100

    assign the higher value to the next position

--- InsertionSort.java
+++ InsertionSort.java
@@ -22,7 +22,10 @@ class InsertionSort {
 			// and check their values against key (A[j]). We do it in reverse order
 			// from the index closer to j
 			while (i >= 0) {
+				// insert key in the correct place
 				if (array[i] > key) {
+					// assign this greater value to the next position i + 1
+					array[i + 1] = array[i];
 					System.out.println(key);
 				}
 				i = i - 1;
