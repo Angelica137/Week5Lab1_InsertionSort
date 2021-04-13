04ce66732c19b4dad19d2a33fe000a627eb1c33a
Angelica <Angelica137@users.noreply.github.com>
2021-04-13 22:46:11 +0100

    iterate through the array

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,15 @@ class InsertionSort {
 	// accepts an array of ints as anrgument and sorts it
 	// in ascending order
 	public int[] sort(int[] array) {
+		// start from the second index since the first index
+		// will be default be the smallest when drawn first
+		// dclare an int to store this index
+		int j;
+		for (j = 1; j < array.length; j++) {
+			System.out.println("I work");
+		}
 		return array;
+
 	}
 
 }
--- TestInsertionSort.java
+++ TestInsertionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class TestInsertionSort {
 	public static void main(String[] args) {
 		InsertionSort i = new InsertionSort();
-		int[] a = { 1, 2, 3 };
+		int[] a = { 10, 7, 15, 5 };
 		System.out.println(Arrays.toString(i.sort(a)));
 
 	}
